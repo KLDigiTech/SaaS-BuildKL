12ebc9899a135145da9d43ffc21557a69e278ac7
digitaluminium <digitaluminium@gmail.com>
2026-03-31 18:40:01 +0200

    mise en place notificatiopn sur toute les pages en html pour l'instant

--- Js/dashboard.js
+++ Js/dashboard.js
@@ -44,22 +44,3 @@ if (ctx) {
   });
 }
 
-//NOTIF 
-
-const notification = document.querySelector('.notif');
-const dropDown = document.getElementById('notifDropdown')
-
-notification.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  if (dropDown.style.display === 'block') {
-    dropDown.style.display = 'none';  // cacher
-  } else {
-    dropDown.style.display = 'block'; // afficher
-  }
-});
-document.addEventListener('click', (e) => {
-    if (!notification.contains(e.target)) {
-        dropDown.style.display = 'none';
-    }
-});
--- Js/script.js
+++ Js/script.js
@@ -6,3 +6,22 @@ btnDeconnexion.addEventListener('click', (e) => {
   sessionStorage.clear();
   window.location.href = '../index.html';
 });
+//NOTIF 
+
+const notification = document.querySelector('.notif');
+const dropDown = document.getElementById('notifDropdown')
+
+notification.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (dropDown.style.display === 'block') {
+    dropDown.style.display = 'none';  // cacher
+  } else {
+    dropDown.style.display = 'block'; // afficher
+  }
+});
+document.addEventListener('click', (e) => {
+    if (!notification.contains(e.target)) {
+        dropDown.style.display = 'none';
+    }
+});
