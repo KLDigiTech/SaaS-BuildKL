f062b21674a5c26454aae429ccaf5da1d1b3bd36
Kévin Level <kldigitech34@gmail.com>
2026-04-28 01:05:36 +0200

    feat: module devis + catalogue réglages

---
+++ Js/devis.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById('add-devis-btn').addEventListener('click', function () {
+
+    const liste = document.querySelector('.devis-list');
+    const form = document.querySelector('.devis-form');
+
+    liste.style.display = 'none';
+    form.style.display = 'flex';
+
+});
